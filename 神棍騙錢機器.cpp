3dab70163fe3055150243a3807edf428f573f07e
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-17 16:28:06 +0800

    Create 神棍騙錢機器.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

---
+++ 神棍騙錢機器.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int M,D,S;
+	cin>>M;
+	cin>>D;
+    S=(M*2+D)%3;
+    if (S==0)
+    cout<<"普通"<<"\n";
+    if (S==1)
+    cout<<"吉"<<"\n"; 
+    if (S==2)
+    cout<<"大吉"<<"\n"; 
+	return 0;
+}
