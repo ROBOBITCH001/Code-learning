2185f446b13a92892a9d1d8472447c1899025336
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-18 09:42:01 +0800

    Create 判斷季節.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

---
+++ 判斷季節.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int k;
+	cin>>k;
+	if (k>=3 and k<=5)
+	cout<<"Spring!\n";
+	else if
+	(k>=6 and k<=8)
+	cout<<"Summer!\n";
+	else if
+	(k>=9 and k<=11)
+	cout<<"Autumn!\n";
+	else 
+	cout<<"Winter!\n";
+//	else if (k>13)
+//	cout<<"go fuck yourself\n";
+}
