ada08343c84962d7c6cfffbfc3e85b1cd43036e5
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-18 14:33:12 +0800

    Create jojo乘法表.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

---
+++ jojo乘法表.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1,n2;
+	for (int n1=1;n1<=9; n1++)被乘數1~9
+	{
+		for (int n2 =1; n2<=9;n2++)乘數1~9
+			{
+				cout<<n1<<'*'<<n2<<'='<<n1*n2<<'\t';//被乘數 * 乘數 = 乘數被乘數(積)
+			}
+			cout<<"\n";
+	}
+	cout<<"\n";
+}
