a95edf25e2f60eaaa1cd790e050ae2cc7adc0908
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-18 11:51:44 +0800

    Create 巢狀for迴圈練習.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

---
+++ 巢狀for迴圈練習.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int c;
+	c=1;
+	for (int i=1;i <=5; i++){
+		for (int j = 1;j<=i; j++)//j會受到i遞增限制，故顯示效果會像階梯
+		{
+			cout<<'*';
+		} 
+			cout<<"\n";
+			
+}
+	}
