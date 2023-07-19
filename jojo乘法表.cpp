4de42217bdec8b79b212602d677ce85b33d70036
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-19 16:01:01 +0800

    Update jojo乘法表.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

--- jojo乘法表.cpp
+++ jojo乘法表.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
 	int n1,n2;
-	for (int n1=1;n1<=9; n1++)被乘數1~9
+	for (int n1=1;n1<=9; n1++)//被乘數1~9
 	{
-		for (int n2 =1; n2<=9;n2++)乘數1~9
+		for (int n2 =1; n2<=9;n2++)//乘數1~9
 			{
 				cout<<n1<<'*'<<n2<<'='<<n1*n2<<'\t';//被乘數 * 乘數 = 乘數被乘數(積)
 			}
