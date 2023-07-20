49aaaf9bcc72923cfe9f46fab30b05195727c6c5
ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>
2023-07-20 13:58:10 +0800

    Create 文字比較器.cpp
    Signed-off-by: ROBOBITCH001 <115913438+ROBOBITCH001@users.noreply.github.com>

---
+++ 文字比較器.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	int mov = '=';
+	string d1,d2;
+	cin>>num>>mov;
+	
+	for (int i=0;i<num; ++i) {
+		cin >> d1 >> d2;
+		
+		cout << d1 << '\n' << d2 << '\n';
+		
+		for (int j=0; j < d1.size();j++)
+			{
+			if (d1[j]==d2[j])
+				cout<<'.';
+			else 
+				cout<<'*';
+			} 
+		cout<<'\n'<<'\n';
+		
+	}
+	
+}
